e321cc3630a3923b2d53c29160955ea42375b500
cyanghan007 <chan_yang@hotmail.com>
2022-08-18 18:54:15 -0400

    made changes to app.js and main.css

--- app.js
+++ app.js
@@ -1,75 +1,106 @@
-class Aside extends React.Component {
-    render(){
+class Content extends React.Component {
+    render() {
         return (
-        <aside>
-            <nav>
-                <a>Dashboard</a>
-                <a> Widget</a>
-                <a> Reviews</a>
-                <a> Customers</a>
-                <a> Online Analysis</a>
-                <a> Setting</a>
-            </nav>
-
-        </aside>
+            <div className="content">
+                <Stats />
+                <WebVisitors />
+            </div>
         )
     }
 }
-
-//used to recyle componets
-
-class Reviews extends React.Component{
-    render () {
-        return <section>Reviews</section>  
-    }
-}
-
-class AvgRating extends React.Component {
-    render(){
-        return <section>Average Rating</section>
-    }
-}
-
-class Sentiment extends React.Component {
-    render(){
-        return <section>Sentiment Analysis</section>
-    }
-}
-
-class WebVistors extends React.Component {
-    render(){
+class Sidebar extends React.Component {
+    render() {
         return (
-            <section id='large'>
-                Website Visitors
-                <div>821</div>
-
-            </section>
-        )
-    }
-}
-
-
-class App extends React.Component{
-    render () {
-        return(
-            <div id='app'>
-                <Aside />
-                <div id='container'>
-                    <div id='top'>
-                        <Reviews />
-                        <AvgRating />
-                        <Sentiment />
-                    </div>
-                    <WebVistors />
+            <div class="Sidebar">
+                <h3>Dashboard</h3>
+                <div class='sidebarcontent'>
+                    <ul>
+                        <li>Widget</li>
+                        <li>Reviews</li>
+                        <li>Customers</li>
+                        <li>Online Analysis</li>
+                        <li>Settings</li>
+                    </ul>
                 </div>
             </div>
         )
     }
 }
 
+class Reviews extends React.Component {
+    render() {
+        return (
+            <div class='Reviews'>
+                <h3>Reviews</h3>
+                <p>1,281</p>
+            </div>
+        )
+    }
+}
+
+class AvgRating extends React.Component {
+    render() {
+        return (
+            <div class='AvgRating'>
+                <h3>Average Rating</h3>
+                <p>4.6</p>
+            </div>
+        )
+    }
+}
+
+class SentAn extends React.Component {
+    render() {
+        return (
+            <div class='SentAn'>
+                <h3>Sentiment Analysis</h3>
+                <ul>
+                    <li>960</li>
+                    <li>122</li>
+                    <li>321</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+class WebVisitors extends React.Component {
+    render() {
+        return (
+            <div className="WebVisitors">
+                <h3>Website Visitors</h3>
+                <p>821</p>
+                <div className="webVisDiv"></div>
+            </div>
+        )
+    }
+}
+
+class Stats extends React.Component {
+    render() {
+        return (
+            <div className="stats">
+                <Reviews />
+                <AvgRating />
+                <SentAn />
+            </div>
+        )
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="app">
+                <Sidebar />
+                <Content />
+            </div>
+        )
+    }
+}
+
+
 ReactDOM.render(
     <App />,
-    document.querySelector('main')
-  );
-
-
+    document.querySelector('.main')
+)
